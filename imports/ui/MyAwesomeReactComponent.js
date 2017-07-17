b0a5427094c11e80dc11a5a6a3450d4ed9727930
Grimmer <grimmer0125@gmail.com>
2017-07-17 16:40:31 +0800

    add missing function, onBrowserClick.

--- imports/ui/MyAwesomeReactComponent.js
+++ imports/ui/MyAwesomeReactComponent.js
@@ -15,6 +15,14 @@ import { Tracker } from 'meteor/tracker';
 import '../api/methods.js';
 import { Responses } from '../api/responses.js';
 
+// const style = {
+//   height: 100,
+//   width: 100,
+//   margin: 20,
+//   textAlign: 'center',
+//   // display: 'inline-block',
+// };
+
 export default class MyAwesomeReactComponent extends Component {
   constructor(props) {
     super(props);
@@ -56,6 +64,20 @@ export default class MyAwesomeReactComponent extends Component {
     });
   }
 
+  onBrowserClick = () => {
+      console.log("open/close file browser")
+
+      Meteor.call('queryFileList', (error, result) => {
+        console.log("get result:", result);
+      });
+
+      if (!this.state.browserOpened) {
+        this.setState({browserOpened: true});
+      } else {
+        this.setState({browserOpened: false});
+      }
+    }
+
   render() {
     const fileItems = this.state.files.map((file, index) => {
       if (file.type === 'fits') {
@@ -79,8 +101,8 @@ export default class MyAwesomeReactComponent extends Component {
     return (
       <div>
         <p>Test Font</p>
-        <RaisedButton label="Primary" primary />
-        <FlatButton label="Secondary" secondary />
+        <RaisedButton onTouchTap={this.onBrowserClick} label="Query File list" primary />
+        <FlatButton label="Test Secondary/accent color" secondary />
         { fileItems && fileItems.length > 0 &&
           <List >
             {fileItems}

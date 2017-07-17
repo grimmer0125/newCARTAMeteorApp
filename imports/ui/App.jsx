3fed05053fc250753bcdb9330187cd6e1e03c321
Grimmer <grimmer0125@gmail.com>
2017-07-17 16:26:41 +0800

    setup accent1Color.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,9 +8,23 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { grey400 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+// ref
+// 1. https://github.com/callemall/material-ui/blob/master/src/styles/getMuiTheme.js
+// 2. http://www.material-ui.com/#/customization/themes
+
+// primaryColor: palette.primary1Color, not effect for RaisedButton, why?
+// primary1Color
+// primaryTextColor: palette.alternateTextColor
+// secondaryColor: palette.accent1Color,
+
+// these primary and accent colosr are from https://grommet.github.io
 const muiTheme = getMuiTheme({
   palette: {
     primary1Color: '#865CD6', // grey400,
+    accent1Color: '#DC2878', // ~ its means is the same as secondary and the following secondary*Color do not take effect
+    // secondaryColor: '#DC2878',
+    //     secondaryTextColor: '#DC2878',
+    //     secondaryIconColor: '#DC2878',
   },
   // appBar: {
   //   height: 50,

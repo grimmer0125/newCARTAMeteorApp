c0ffcdcbe76130b713205938e039074f3f8c6b96
Julie Yao <mhyao@asiaa.sinica.edu.tw>
2017-08-23 16:35:47 +0800

    histogram file

---
+++ imports/app/Histogram.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Histogram extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <p>Histogram</p>
+      </div>
+    );
+  }
+}

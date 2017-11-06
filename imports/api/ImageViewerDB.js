dba014a269511279798dbe7e8180c56954fa02fd
Grimmer <grimmer0125@gmail.com>
2017-11-06 18:29:35 +0800

    rename db name, imageviewer to imageviewerdb.

--- imports/api/ImageViewerDB.js
+++ imports/api/ImageViewerDB.js
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
-export const ImageViewerDB = new Mongo.Collection('imageviewer');
-ImageViewerDB.cartaSet = 'imageviewer';
+export const ImageViewerDB = new Mongo.Collection('imageviewerdb');
+ImageViewerDB.cartaSet = 'imageviewerdb';
 
 if (Meteor.isServer) {
   Meteor.publish(ImageViewerDB.cartaSet, sessionID => ImageViewerDB.find({ sessionID }));

60f7ae99eea1a1ae8f8a63b60066785ba747ee90
Grimmer <grimmer0125@gmail.com>
2017-11-07 14:46:22 +0800

    remove duplicate buttons.

--- imports/fileBrowser/FileBrowser.js
+++ imports/fileBrowser/FileBrowser.js
@@ -170,7 +170,6 @@ class FileBrowser extends Component {
               {fileItems}
             </SelectableList>
             <RaisedButton style={buttonStyle} onTouchTap={this.readImage} label="Read" secondary />
-            <RaisedButton style={buttonStyle} label="close" secondary />
           </div>
         }
         <RaisedButton style={buttonStyle} onTouchTap={this.closeImage} label="close" secondary />
